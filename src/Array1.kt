1693410a94b0e1402a0dbd0c0a68177dcf5dd1a1
Hakimxon <root5675520@gmail.com>
2023-01-31 21:55:46 +0500

    dars

--- src/Array1.kt
+++ src/Array1.kt
@@ -1,12 +1,18 @@
 import java.lang.reflect.Array
 
 fun main() {
+    var a = "sdsfs"
+    println(a::class.simpleName)
+// or
+    println(a::class.qualifiedName) // kotlin.Int
     print("n = ")
     val n = readLine()!!.toInt()
     val array =  IntArray(n)
+//    val array2 =  IntArray(1,3,4,5,6)
     for (i in 0 until n){
         array[i] = readLine()!!.toInt()
     }
+//    print(IntArray.)
     for (i in 0 until n){
         print("${array[i]} ")
     }
--- src/Main.kt
+++ src/Main.kt
@@ -1,6 +1,25 @@
 fun main() {
-    val x: Int = 20
-    val y: Int = 50
+//    val digits: IntRange = 1..99
+//    for (i in digits)
+    var x: Int = 20
+    var y: Int = 50
+    val s1 = x + y
+    val s2 = x.plus(y)
+    val s3 = x - y
+    val s4 = x.minus(y)
+    val s5 = x * y
+    val s6 = x.times(y)
+    val s7 = x / y
+    val s8 = x.div(y)
+    val s9 = x % y
+    val s0 = x.rem(y)
+    val max = x.coerceAtLeast(y)
+    val min = x.coerceAtMost(y)
+//    x.plusAssign(y) x+=y
+    println(x.dec()) //x++
+    println(x.inc()) //y--
+
+
     val yigindi = x + y
     val ayirma = x - y
     val kupaytma = x * y
